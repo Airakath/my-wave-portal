b48c880a275cd6a886448df68d49754352de0564
Nicolas Bernin <nicolas.bernin@reccat.io>
2022-09-21 21:06:57 +0200

    Storing messages from users on the blockchain

--- contracts/WavePortal.sol
+++ contracts/WavePortal.sol
@@ -5,15 +5,50 @@ pragma solidity ^0.8.4;
 import "hardhat/console.sol";
 
 contract WavePortal {
-    uint256 totalWaves;
+    uint256 totalWaves;    
+    event NewWave(address indexed from, uint256 timestamp, string message);
+
+    struct Wave {
+        address waver;
+        string message;
+        uint256 timestamp;
+    }
+
+    Wave[] waves;
 
     constructor() {
         console.log("Yo yo, I am a contract and I am smart");
     }
 
-    function wave() public {
+    function wave(string memory _message) public {
         totalWaves += 1;
+
         console.log("%s has waved!", msg.sender);
+        waves.push(Wave(msg.sender, _message, block.timestamp));
+
+        emit NewWave(msg.sender, block.timestamp, _message);
+
+        uint256 prizeAmount = 0.0001 ether;
+        require(
+            prizeAmount <= address(this).balance,
+            "Trying to withdraw more money than the contract has."
+        );
+        (bool success, ) = (msg.sender).call{value: prizeAmount}("");
+        require(success, "Failed to withdraw money from contract.");
+    }
+    /*
+    function getCallerByWave(uint256 waveNumber) public view returns (address) {
+        return ;
+    }
+    */
+    /*
+    function getTotalCallers() public view returns (address[] memory) {
+        return listCallers;
+    }
+    */
+
+    function getAllWaves() public view returns (Wave[] memory) {
+        return waves;
     }
 
     function getTotalWaves() public view returns (uint256) {
